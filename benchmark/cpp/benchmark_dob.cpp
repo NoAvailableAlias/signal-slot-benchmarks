d1e2aa3a7396cb75cf0e6ec8a254201269ec3dd3
NoAvailableAlias <apek.blog@gmail.com>
2018-08-13 19:51:07 -0400

    adding dacap observable lib

---
+++ benchmark/cpp/benchmark_dob.cpp
@@ -0,0 +1,34 @@
+#include "../hpp/benchmark_dob.hpp"
+
+NOINLINE(void Dob::initialize())
+{
+    // NOOP
+}
+NOINLINE(void Dob::validate_assert(std::size_t N))
+{
+    return Benchmark<Signal, Dob>::validation_assert(N);
+}
+NOINLINE(double Dob::construction(std::size_t N))
+{
+    return Benchmark<Signal, Dob>::construction(N);
+}
+NOINLINE(double Dob::destruction(std::size_t N))
+{
+    return Benchmark<Signal, Dob>::destruction(N);
+}
+NOINLINE(double Dob::connection(std::size_t N))
+{
+    return Benchmark<Signal, Dob>::connection(N);
+}
+NOINLINE(double Dob::emission(std::size_t N))
+{
+    return Benchmark<Signal, Dob>::emission(N);
+}
+NOINLINE(double Dob::combined(std::size_t N))
+{
+    return Benchmark<Signal, Dob>::combined(N);
+}
+NOINLINE(double Dob::threaded(std::size_t N))
+{
+    return Benchmark<Signal, Dob>::threaded(N);
+}
---
+++ benchmark/hpp/benchmark_dob.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "../lib/dacap/obs/dacap_connection.h"
+#include "../lib/dacap/obs/dacap_signal.h"
+
+#include "../../benchmark.hpp"
+
+class Dob
+{
+    obs::scoped_connection reg;
+
+    NOINLINE(void handler(Rng& rng))
+    {
+        volatile std::size_t a = rng(); (void)a;
+    }
+
+    public:
+
+    using Signal = obs::signal<void(Rng&)>;
+
+    template <typename Subject, typename Foo>
+    static void connect_method(Subject& subject, Foo& foo)
+    {
+        foo.reg = subject.connect(&Foo::handler, &foo);
+    }
+    template <typename Subject>
+    static void emit_method(Subject& subject, Rng& rng)
+    {
+        subject(rng);
+    }
+
+    // Only used by a few benchmark classes
+    static void initialize();
+
+    static void validate_assert(std::size_t);
+    static double construction(std::size_t);
+    static double destruction(std::size_t);
+    static double connection(std::size_t);
+    static double emission(std::size_t);
+    static double combined(std::size_t);
+    static double threaded(std::size_t);
+
+    // The following is used for report outputs
+    static constexpr const char* C_LIB_NAME = "* dacap observable";
+    static constexpr const char* C_LIB_SOURCE_URL = "https://github.com/dacap/observable";
+    static constexpr const char* C_LIB_FILE = "benchmark_dob";
+    static constexpr const char* C_LIB_IS_HEADER_ONLY = "-";
+    static constexpr const char* C_LIB_DATA_STRUCTURE = "std::vector";
+    static constexpr const char* C_LIB_IS_THREAD_SAFE = "X";
+};
